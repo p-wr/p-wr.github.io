ac2e04ae089ec87d0018539346598a8780b5e75b
Shachar Itzhaky <corwin.amber@gmail.com>
2020-03-24 21:11:07 +0200

    Updated jsCoq (0.11.0).

--- scripts/update-ssar.sh
+++ scripts/update-ssar.sh
@@ -4,8 +4,8 @@ here=`pwd`
 lab=~/var/workspace/Web.Verify.Coq
 repo=~/var/workspace/Teaching.SSAR
 
-sed 's/dev-only.*//' $lab/src/lab.html | sed 's/ssar =.*/ssar = ".\/repo";/' | \
-    sed 's/\.\.\/node_modules\/jscoq/..\/jscoq/' > ./ssar/lab.html
+sed 's/dev-only.*//' $lab/src/lab.html | sed 's/ssar =.*/ssar = ".\/repo";/'  \
+     > ./ssar/lab.html
 
 sed 's/\.\.\/img/.\/img/' $lab/src/lab.css > ./ssar/lab.css
 
