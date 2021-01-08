48f9fa14c672d09f93013e6f9a61e05bd5ed5ef5
Shachar Itzhaky <corwin.amber@gmail.com>
2021-01-08 22:47:54 +0200

    Tidy info html.

--- scripts/moodle-touchup.js
+++ scripts/moodle-touchup.js
@@ -1,5 +1,6 @@
 const fs = require('fs'),
-      child_process = require('child_process');
+      child_process = require('child_process'),
+      cheerio = require('cheerio');
 
 
 function sh(cmd) {
@@ -18,15 +19,32 @@ function renameCss() {
     sh('find . -name all-rtl -exec mv {} {}.css \\;');
 }
 
-function updateHtml(filename) {
-    var txt = fs.readFileSync(filename, 'utf-8');
-    fs.writeFileSync(filename, 
-        txt.replace(/\/icon"/g, '/icon.svg"')
-           .replace(/\/pdf"/g, '/pdf.svg"')
-           .replace(/\/all-rtl"/g, '/all-rtl.css"'));
+function tidyHtml(infilename, outfilename) {
+    var txt = fs.readFileSync(infilename, 'utf-8'),
+        $ = cheerio.load(txt);
+
+    for (let a of $('link')) {
+        var href = $(a).attr('href');
+        if (href && href.endsWith('/all-rtl'))
+            $(a).attr('href', href + '.css');
+        else if (href.includes('?'))
+            $(a).remove();
+    }
+
+    for (let a of $('img')) {
+        var src = $(a).attr('src');
+        if (src && src.match(/\/icon|pdf$/))
+            $(a).attr('src', src + '.svg');
+    }    
+
+    $('nav, script').remove();
+    $('section[data-region="blocks-column"]').remove();
+
+    fs.writeFileSync(outfilename, $.html());
 }
 
 renameIcons();
 renameCss();
-updateHtml('ssar/info/course/236347.html');
+tidyHtml('ssar/info/course/view.php?id=5865',
+         'ssar/info/course/236347.html');
 
